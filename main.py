403ee24afe1343dfb8f1268c4cf4c65cd99e20da
unknown <yellowfox2@protonmail.com>
2020-12-28 23:21:38 -0500

    cleanup + mainmenu

--- main.py
+++ main.py
@@ -1,56 +1,60 @@
 import statsapi
 import json
 import re
+import argparse
 
-def get_players(year):
-    print("Getting player IDs....")
-    playersDict = {}
-    players = statsapi.get('sports_players',{'season':year})
+def appendYearlyPlayerStats(playersDict,playerID,season,stats):
+    statsDict = {}
+    statsDict[season] = stats
+    playersDict[playerID][season] = statsDict[season]
 
-    for people in players['people']:
-        playersDict[people.get('id')] = {}
-        playersDict[people.get('id')]['fullName'] = people.get('fullName')
-        playersDict[people.get('id')]['pos'] = people['primaryPosition'].get('abbreviation')
-
-    return playersDict
-        
-def getYearlyHittingStats(playerID):
-    for years in statsapi.player_stat_data(playerID, type="yearByYear")['stats']:
-        if(years.get('group') == 'hitting'):
-            print(years.get('season'))
-            print(json.dumps(years.get('stats'),indent=4))
-
-def getYearlyPitchingStats(playerID):
-    for years in statsapi.player_stat_data(playerID, type="yearByYear")['stats']:
-        if(years.get('group') == 'pitching'):
-            print(years.get('season'))
-            print(json.dumps(years.get('stats'),indent=4))
+def getYearlyStats(playerID,playersDict):
+    for years in statsapi.player_stat_data(playerID, type='yearByYear')['stats']:
+        if(years.get('group') == playersDict[playerID]['type']):
+            appendYearlyPlayerStats(playersDict,playerID,years.get('season'),years.get('stats'))
 
 def checkForPlayerNames(playerDict,playerInput):
     foundPlayerNames = {}
     for player in playerDict:
-        if re.search(rf"\b(?=\w){playerInput}\b(?!\w)",playerDict[player]['fullName'],flags=re.IGNORECASE):
+        if re.search(rf'\b(?=\w){playerInput}\b(?!\w)',playerDict[player]['fullName'],flags=re.IGNORECASE):
             foundPlayerNames[player] = (playerDict[player]['fullName'])
     return foundPlayerNames
 
-def searchForPlayers(playersDict):
-    playerInput = input("find yearly stats of player: ")
-    names = {}
-    names = checkForPlayerNames(playersDict,playerInput)
-    print(f'Players found: {names}')
-    for key in names.keys():
-        if playersDict[key]['pos'] == 'P':
-            print("Pitcher found")
-            getYearlyPitchingStats(key)
-        else:
-            print("Batter found")
-            getYearlyHittingStats(key)
+def userSearchForYearlyPlayerStats(playersDict):
+    playerInput = input('Find yearly stats of player ("quit" to exit search): ')
+    if playerInput != 'quit':
+        names = {}
+        names = checkForPlayerNames(playersDict,playerInput)
+        print(f'\nPlayers found: {names}\n')
+        for key in names.keys():
+            getYearlyStats(key,playersDict)
+            print(json.dumps(playersDict[key],indent=4))
+        return True
+    else:
+        print('\nExiting....\n')
+        return False
+  
+def get_players(year):
+    print(f'\nGetting player IDs from {year}....\n')
+    playersDict = {}
+    players = statsapi.get('sports_players',{'season':year})
+    for people in players['people']:
+        playersDict[people.get('id')] = {}
+        playersDict[people.get('id')]['fullName'] = people.get('fullName')
+        playersDict[people.get('id')]['pos'] = people['primaryPosition'].get('abbreviation')
+        playersDict[people.get('id')]['type'] = 'hitting' if playersDict[people.get('id')]['pos'] != 'P' else 'pitching'
+    return playersDict
 
 def main():
-    currentYear = 2020
-    players = get_players(currentYear)
-    searchForPlayers(players)
-    #print(players)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-y', '--year')
+    parser.add_argument('-p', '--playerName')
+    parser.add_argument('-t', '--teamName')
+    args = parser.parse_args()
+    players = get_players(2020) if args.year is None else get_players(args.year)
+    while userSearchForYearlyPlayerStats(players):
+        print('\n==Main Menu==\n')
+        
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
